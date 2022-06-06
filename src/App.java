ad1277d28fe56dc72cd3d58c947e70a90d276b95
TeodorGruia <teodorgruia@gmail.com>
2022-06-06 12:02:05 -0400

    Init Commit on Branch

--- src/App.java
+++ src/App.java
@@ -3,8 +3,13 @@ public class App {
         //Given two payments, estimate total miles earned
         Double p1 = 25.00;
         Double p2 = 45.50;
+        double Miles = totalMilesEarned(p1, p2);
 
-        Double totalMilesEarned = (p1 * 2) + (p2 * 2);
-        System.out.println(totalMilesEarned);
+        System.out.println("Congrats! You've earned: " + Miles + " Miles!");
+    
+    }
+    public static double totalMilesEarned(double pay1, double pay2) {
+        return (pay1*2) + (pay2*2);
+
     }
 }
